299561c83dc03a2c413b62430f1cc6759f1fa17a
MrzTibbs <mrztibbs@gmail.com>
2022-10-30 16:01:07 -0500

    Finally completed the troubleshooting to get the webpages to show. I broke some of the code I had to get the page to show. Committing what I have so far

--- src/main/java/org/launchcode/techjobs/mvc/Application.java
+++ src/main/java/org/launchcode/techjobs/mvc/Application.java
@@ -2,8 +2,12 @@ package org.launchcode.techjobs.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@ComponentScan("org.launchcode.techjobs.mvc.controllers")
+@EnableWebMvc
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/org/launchcode/techjobs/mvc/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/HomeController.java
@@ -3,7 +3,6 @@ package org.launchcode.techjobs.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 
@@ -13,16 +12,13 @@ import java.util.HashMap;
 @Controller
 public class HomeController {
 
-    @GetMapping(value = "")
+    @GetMapping("")
     public String index(Model model) {
-
         HashMap<String, String> actionChoices = new HashMap<>();
         actionChoices.put("search", "Search");
         actionChoices.put("list", "List");
-
         model.addAttribute("actions", actionChoices);
 
         return "index";
     }
-
 }
