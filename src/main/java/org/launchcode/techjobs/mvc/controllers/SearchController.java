8d72a575327e1a13f045c467406d7c2e0ce22d60
April G Tibbs <mrztibbs@gmail.com>
2022-11-10 18:33:26 -0600

    I found an issue in using the search a second time in a row. Corrected it by "refreshing" the columns attribute on the model.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -27,23 +27,29 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+
+
     @PostMapping("results")
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam (required = false) String searchTerm){
         ArrayList<Job> jobs;
-        String title = "Jobs With All: " + searchTerm;
+        String title;
         try {
-            if (searchType.toLowerCase().trim().equals("all") && searchTerm.toLowerCase().trim().equals("all") || searchTerm.equals("")) {
-                jobs = JobData.findAll();
+            //findAll if the searchType is all and searchTerm is all/blank
+            if (searchType.toLowerCase().trim().equals("all") && searchTerm.toLowerCase().trim().equals("all") ||
+                    searchTerm.equals("")) {
+                title = "Jobs With " + columnChoices.get(searchType) + ": All";
+                model.addAttribute("title", title);
+                model.addAttribute("jobs", JobData.findAll());
             } else {
                 jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+                title = "Jobs With " + columnChoices.get(searchType) + ": " + searchTerm;
+                model.addAttribute("title", title);
+                model.addAttribute("jobs", jobs);
             }
-
-            model.addAttribute("title", title);
-            model.addAttribute("jobs", jobs);
             model.addAttribute("columns", columnChoices);
-        } catch (Exception e) {
-            Error error = new Error ("Sorry! No jobs found!");
-            throw error;
+        }catch (Exception e) {
+            Error err = new Error("Sorry No jobs found!");
+            throw err;
         }
         return "search";
     }
